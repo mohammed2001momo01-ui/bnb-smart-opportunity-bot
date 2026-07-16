544b81c176cb22a08f6a38c9d6f191aa4e7aaf2c
momohammed <mohammed2001momo01@gmail.com>
2026-07-16 05:07:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
 
+import time
+
+from scanner import find_opportunities
+from telegram_bot import send_alert
+
+
+def run():
+    sent = set()
+
+    while True:
+        opportunities = find_opportunities()
+
+        for opportunity in opportunities:
+            token = opportunity["token"]
+
+            if token not in sent:
+                send_alert(opportunity)
+                sent.add(token)
+
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    run()
